6dde839ba51b1846415661f4e0ae99edea0fd6a7
Ibzi12 <128941663+Ibzi12@users.noreply.github.com>
2023-12-03 08:00:38 +1100

    Add files via upload

--- Let-chat-web-app.js
+++ Let-chat-web-app.js
@@ -3,5 +3,15 @@ function addUser() {
 
     localStorage.setItem("user_name", user_name);
 
-     window.location="Let-chat-web-app_room.html"
+     window.location="home_page";
 }
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVfHED0srcNgp5MT2YKte0-ZLNqhTqSpM",
+    authDomain: "classtest-4902e.firebaseapp.com",
+    projectId: "classtest-4902e",
+    storageBucket: "classtest-4902e.appspot.com",
+    messagingSenderId: "126564935653",
+    appId: "1:126564935653:web:5d992ba90e233d4a20710c"
+  };
+
