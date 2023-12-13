88d09ca9096f06cb6a0e130db4fdebacd3ce55ab
Ibzi12 <128941663+Ibzi12@users.noreply.github.com>
2023-12-13 16:53:28 +1100

    Add files via upload

--- Let-chat-web-app.js
+++ Let-chat-web-app.js
@@ -1,21 +1,7 @@
 function addUser() {
-    user_name=document.getElementById("user_name").value;
+  user_name=document.getElementById("user_name").value;
 
-    localStorage.setItem("user_name", user_name);
+  localStorage.setItem("user_name", user_name);
 
-     window.location="Let-chat-web-app_room.html";
+   window.location="Let-chat-web-app_room.html";
 }
-
-var firebaseConfig = {
-  apiKey: "AIzaSyCpZ7YIEDWmhDG05kZmd-iyPQ0PKw_HOg4",
-  authDomain: "classtest-7e8d8.firebaseapp.com",
-  databaseURL: "https://classtest-7e8d8-default-rtdb.firebaseio.com",
-  projectId: "classtest-7e8d8",
-  storageBucket: "classtest-7e8d8.appspot.com",
-  messagingSenderId: "605841517622",
-  appId: "1:605841517622:web:7a6c9e6df93047efc87c97",
-  measurementId: "G-V1VRT0H00X"
-};
-
-// Initialize Firebase
-firebaseConfig.initializeApp(firebaseConfig);
